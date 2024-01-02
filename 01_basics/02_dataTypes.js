0c51ead59b5b1fc982f4214b9a313fac51bccff3
mursaleenismail <127970348+mursaleenismail@users.noreply.github.com>
2024-01-02 06:27:09 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+//alert ( 3+2) we are using node js not browser
+
+console.log(3+3); // code readability should be high
+console.log("World");
+
+let name ="Mursalin"
+let age = 25
+let isLoggedIn=false
+
+
+
+// number  => 2 to power of 23
+// bigint
+// strimg => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+console.log(typeof undefined); //undefined
+console.log(typeof null);// object
