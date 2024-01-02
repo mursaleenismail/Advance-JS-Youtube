751058e3b9a59767b93788cb5c737fb7e132ce95
mursaleenismail <127970348+mursaleenismail@users.noreply.github.com>
2024-01-02 07:12:22 +0000

    Conersion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score="World"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "77" => 77
+// "77xyz" => NaN ( Not A Number)
+// true => 1; flase =>0
+
+
+let isLoggedIn= "";
+
+let booleanIsLoggedIn = Boolean ( isLoggedIn)
+// console.log( booleanIsLoggedIn );
+
+// 1 => true; 0>= false
+// " " => false
+// "Mursalin" => true
+
+let someNumber=65
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
