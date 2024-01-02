3a616c1e53a125786b2f472cfa2d0470fa607a79
mursaleenismail <127970348+mursaleenismail@users.noreply.github.com>
2024-01-02 09:20:05 +0000

    conversion of operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -24,5 +24,49 @@ let booleanIsLoggedIn = Boolean ( isLoggedIn)
 let someNumber=65
 
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+
+ // ********************  Operations  ***********************//
+
+
+let value = 9
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**6);
+// console.log(2/6);
+// console.log(2%6);
+
+
+let str1 = "hello"
+let str2 = " Mursalin"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "4");
+
+// console.log( (4+5) * 6  % 3 );
+// console.log(  54  % 9 );
+
+// console.log(+true);
+// console.log(false);
+
+let num1 , num2 ,num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter= 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
