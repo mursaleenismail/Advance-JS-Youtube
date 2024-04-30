79febbb06c96636c93934516c53ba457642972c4
mursaleenismail <127970348+mursaleenismail@users.noreply.github.com>
2024-04-30 07:08:08 +0000

    Objects In jS

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,35 @@
+// constructor operators
+
+
+//  object literals
+  
+const mySym = Symbol("Key1")
+
+const JsUser ={
+    name :"Mursaleen",
+    "full name" :"Mursaleen Ismail",
+    age:22,
+    [mySym]:"myKey1",
+    email:"mursaleen@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:[ "Monday","Saturday" ]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser [mySym])
+
+JsUser.email="mursaleendev@gmail.com"
+// console.log(JsUser);
+Object.freeze(JsUser);
+JsUser.email="mursaleennoor@gmail.com"
+// console.log(JsUser);
+
+
+JsUser.greeting= function(){
+    console.log("Hello JS User");
+}
+
+console.log(JsUser.greeting);
+
