55f3b9b943a1ced9d34644098af22169bb364667
mursaleenismail <127970348+mursaleenismail@users.noreply.github.com>
2024-05-01 07:47:58 +0000

    Objects In JS

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,6 +27,35 @@ const obj2 = { 3: "a", 4: "b" }
 const obj3 = { 5: "a", 6: "b" }
 
 // const obj4 = { obj1, obj2, obj3 }
-const obj4 = Object.assign({},obj1,obj2,obj3)
+// const obj4 = Object.assign({},obj1,obj2,obj3)
 
-console.log(obj4);
+const obj4 = {...obj1 ,...obj2,...obj3}
+
+// console.log(obj4);
+
+
+
+const Users=[
+    {
+        id:"1",
+        email:"h@gmail.com"
+    },
+    {
+        id:"2",
+        email:"h@gmail.com"
+    },
+    {
+        id:"3",
+        email:"h@gmail.com"
+    }
+]
+
+// console.log(Users[1].email);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn") );
