ecc1f02f49dec1b6e2fb1380ab3d951d9d251f9c
mursaleenismail <127970348+mursaleenismail@users.noreply.github.com>
2024-05-01 07:23:41 +0000

    Objects In JS

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,32 @@
+// const tinderUser = new Object ()
+
+const tinderUser = {}
+tinderUser.id = "123Abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+
+
+// console.log(tinderUser);
+
+
+const regularUser = {
+    email: "sammy@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Mursaleen",
+            lastname: "Ismail"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname);
+
+const obj1 = { 1: "a", 2: "b" }
+const obj2 = { 3: "a", 4: "b" }
+const obj3 = { 5: "a", 6: "b" }
+
+// const obj4 = { obj1, obj2, obj3 }
+const obj4 = Object.assign({},obj1,obj2,obj3)
+
+console.log(obj4);
